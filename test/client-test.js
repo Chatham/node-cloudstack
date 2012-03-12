583e0dc820857d9f8b973bb7c77cab0b9f889674
Tim Savery <tim.savery@gmail.com>
2012-03-12 15:51:49 -0400

    emitter

--- test/client-test.js
+++ test/client-test.js
@@ -1,18 +1,25 @@
-var client = require('../lib/client');
+var client = require('../lib/cloudstackClient');
 
 var args = (process.env.LIVE)
 	? require('./live/clientargs.json')
 	: require('./nonlive/clientargs.json');
 
-if (!process.env.LIVE) {
+if (!process.env.LIVE) {	
 	var mocks = require('./nonlive/mocks');
 	(new mocks()).engage();
 }
 
-exports['listVirtualMachines'] = function(test) {
-	new client(args).listVirtualMachines(function(res) {	
-		test.equal(res.status, 'ok');
-		test.deepEqual(res.data, {});
+var client = new client(args);
+
+exports['deployVirtualMachine'] = function(test) {
+	var deployment = client.deployVirtualMachine(211, 1, 1);
+
+	deployment.on('done', function(vm) {		
+		test.done();
+	});
+
+	deployment.on('error', function(err) {
+		test.fail('error: ' + err);
 		test.done();
 	});
 };
--- test/nonlive/mocks.js
+++ test/nonlive/mocks.js
@@ -2,10 +2,25 @@ var nock = require('nock');
 
 module.exports = function mocks() {
 	this.engage = function() {
-		nock("http://controller:8080")
-			.get("/client/api?account=TestUser&apiKey=TestUserApiKey&command=listVirtualMachines&domainId=1&response=json&signature=acSwdq7p634LFmIvR3em%2FZzlKEE%3D")
+		nock("http://host:8080")
+			.get('/client/api?account=TestUser&apiKey=TestUserApiKey&command=deployVirtualMachine&domainId=1&response=json&serviceOfferingId=1&templateId=211&zoneId=1&signature=7wdCP6pJhVHL4ULQJzyh60Xd9Nw%3D')
 			.reply(200, {
-				listvirtualmachinesresponse: {}
+				deployvirtualmachineresponse: {
+					jobid: 123,
+					id: 456
+				}
+			})
+			.get('/client/api?account=TestUser&apiKey=TestUserApiKey&command=queryAsyncJobResult&domainId=1&jobid=123&response=json&signature=mpgukeS%2FQWAX1vI8%2FhmFQY9NCsU%3D')
+			.reply(200, {
+				queryasyncjobresultresponse: {
+					jobstatus: 1
+				}
+			})
+			.get('/client/api?account=TestUser&apiKey=TestUserApiKey&command=listVirtualMachines&domainId=1&id=456&response=json&signature=DDmoVEvTZpyN10RQUh3pOz9zYjQ%3D')
+			.reply(200, {
+				listvirtualmachinesresponse: {
+					id: 456
+				}
 			});
 	};
 };
